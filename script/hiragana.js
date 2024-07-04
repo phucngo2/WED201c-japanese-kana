e0332706e78b6cefe23f6947c55f8b41772ec4a3
phucngo2 <phucnnh21@gmail.com>
2024-07-04 20:57:18 +0700

    remove expired links and format code

--- script/hiragana.js
+++ script/hiragana.js
@@ -1,5 +1,5 @@
 function displayGif(letter) {
-    var x = "<img src=\"img/Hira/" + letter + ".gif\">";
-    x = x + "<div class=\"pronounce\">Pronounce: " + letter + "</div>";
-    document.getElementById("hiragif").innerHTML = x;
+  var x = '<img src="img/Hira/' + letter + '.gif">';
+  x = x + '<div class="pronounce">Pronounce: ' + letter + "</div>";
+  document.getElementById("hiragif").innerHTML = x;
 }
